911f3dce57282a6118fa1b415cfd16cd9fff9332
Alex <Alex@AlexDesktop>
2012-10-10 01:30:18 +0200

    cleaning...

--- AndroidAmpSim/obj/local/armeabi/objs/native/native.o.d
+++ AndroidAmpSim/obj/local/armeabi/objs/native/native.o.d
@@ -1,4 +1,130 @@
 obj/local/armeabi/objs/native/native.o: jni/native.c \
- /cygdrive/c/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/pthread.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/time.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/time.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/time.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/signal.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/signal.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/sigcontext.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sched.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_Platform.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_Android.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_AndroidConfiguration.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_AndroidMetadata.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/asset_manager.h \
+ C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/asset_manager_jni.h
 
-/cygdrive/c/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h:
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/pthread.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/time.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/time.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/time.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/signal.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/signal.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/sigcontext.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sched.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_Platform.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_Android.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_AndroidConfiguration.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/SLES/OpenSLES_AndroidMetadata.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/asset_manager.h:
+
+C:/Users/Alex/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/asset_manager_jni.h:
